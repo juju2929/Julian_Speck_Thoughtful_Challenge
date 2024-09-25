33bdb62711555437bfde05a2d59ba612db54b5d5
Julian <julianvictorspeck@gmail.com>
2024-09-25 15:57:06 +1200

    fix task

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from robocorp.workitems import Inputs, Outputs
+from RPA.Robocorp.WorkItems import WorkItems
 from news_scraper import NewsBot
 from robocorp.tasks import task
 import logging
@@ -8,16 +8,15 @@ def run_task():
     # Configure logging
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    # Automatically load the first input work item
-    inputs = Inputs()
-
     try:
-        inputs = Inputs()
-        for item in inputs:
-            payload = item.payload
-            search_phrase = payload.get("search_phrase", "tech")
-            sort_category = payload.get("sort_category", "date")
-            num_months = int(payload.get("num_months", 2))
+        work_items = WorkItems()
+        work_items.get_input_work_item()
+
+        # Extract payload from the current work item
+        payload = work_items.get_work_item_variables()
+        search_phrase = payload.get("search_phrase", "tech")
+        sort_category = payload.get("sort_category", "date")
+        num_months = int(payload.get("num_months", 2))
 
         # Log the received variables
         logging.info(f"Search Phrase: {search_phrase}, Sort Category: {sort_category}, Number of Months: {num_months}")
